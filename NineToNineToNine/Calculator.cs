327fa1b24111949788767b0da108df6c76b44b09
Andreas Summer <summer@sansoft.at>
2023-04-22 23:19:41 +0200

    Add: Calculation in own class Add: Better formating Add: Button in Color Add: Button with Symbol

--- NineToNineToNine/Calculator.cs
+++ NineToNineToNine/Calculator.cs
@@ -3,55 +3,39 @@ using System.CodeDom;
 using System.Diagnostics.Tracing;
 using System.IO;
 using System.Numerics;
+using System.Runtime.InteropServices.JavaScript;
 using System.Threading;
 using System.Threading.Tasks;
 
 namespace NineToNineToNine;
 
-public partial class MainWindow
+public class Calculator
 {
 
     public event EventHandler<CalcEvent> Progress = (sender, args) => { };
+    public event EventHandler<CalcEvent> End = (sender, args) => { };
 
-    internal async Task HeavyMethodAsync(int w1, int w2, int w3, int n, BigInteger sum, string filename,
+    internal async Task StartCalcualte(int w1, int w2, int w3, int startIteration, BigInteger sum, string filename,
     CancellationToken cancelToken)
     {
         DateTime start = DateTime.Now;
         BigInteger quotientTop = BigInteger.Pow(w2, w3);
+        int n;
 
-        for (n = 1; n <= quotientTop - 1; n++)
+        for (n = startIteration; n <= quotientTop - 1; n++)
         {
             sum = BigInteger.Multiply(sum, w1);
-
-            if (n % 1000 == 0) //nur jedes 1000 mal aktualisieren
-            {
-                this.Dispatcher.Invoke(() =>
-                {
-                    Progress(this, new CalcEvent(n, sum));
-                    this.tf_interation.Text = n.ToString("0,000");
-                    if (chb_viewResult.IsChecked ?? false)
-                        this.tf_Result.Text = sum.ToString("0,000");
-                    tf_Digits.Text = sum.ToString().Length.ToString("0,000");
-                    td_Duration.Text = (DateTime.Now.Subtract(start)).ToString();
-                });
-            }
-
+            Progress(this, new CalcEvent(n, quotientTop, sum));
 
             await WriteCurrentStatus(filename, n, quotientTop, sum);
 
-
             if (cancelToken.IsCancellationRequested)
-            {
-                await WriteCurrentStatus(filename, n, quotientTop, sum);
                 cancelToken.ThrowIfCancellationRequested();
-            }
         }
 
         await WriteCurrentStatus(filename, n, quotientTop, sum);
-
-        this.Dispatcher.Invoke(() => { tf_interation.Text = quotientTop.ToString(); });
+        End(this, new CalcEvent(n, quotientTop, sum));
     }
-
 
     private async Task WriteCurrentStatus(string filename, int n, BigInteger quotientTop, BigInteger sum)
     {
@@ -63,16 +47,17 @@ public class CalcEvent : EventArgs
 {
     public CalcEvent()
     {
-            
+
     }
 
-    public CalcEvent(int interation, BigInteger result )
+    public CalcEvent(int interation, BigInteger quotioenTop, BigInteger result)
     {
         Interation = interation;
+        QuotioenTop = quotioenTop;
         Result = result;
     }
 
     public int Interation { get; set; }
     public BigInteger Result { get; set; }
-
+    public BigInteger QuotioenTop { get; set; }
 }
--- NineToNineToNine/MainWindow.xaml.cs
+++ NineToNineToNine/MainWindow.xaml.cs
@@ -1,9 +1,11 @@
 ﻿using System;
+using System.Collections;
 using System.Collections.Generic;
 using System.Diagnostics;
 using System.IO;
 using System.Linq;
 using System.Numerics;
+using System.Security.AccessControl;
 using System.Threading;
 using System.Threading.Tasks;
 using System.Windows;
@@ -16,23 +18,64 @@ namespace NineToNineToNine
     /// </summary>
     public partial class MainWindow : Window
     {
-        CancellationTokenSource? _cancellationTokenSource;
+        private CancellationTokenSource? _cancellationTokenSource;
+        private readonly Calculator _calculator;
+        private DateTime _start = DateTime.MinValue;
+        private DateTime _lastRefresh = DateTime.MinValue;
+
         public MainWindow()
         {
             InitializeComponent();
+            this.tf_startTime.Text = DateTime.Now.ToString();
+
             tf_W2_TextChanged(this, null);
+            _calculator = new Calculator();
+            _calculator.Progress += _calculator_Progress;
+            _calculator.End += _calculator_End; ;
+        }
+
+        private void _calculator_Progress(object? sender, CalcEvent e)
+        {
+            var totalSeconds = DateTime.Now.Subtract(_lastRefresh).TotalSeconds;
+            if (totalSeconds >= 2)
+            {
+                DispayResult(e);
+                _lastRefresh = DateTime.Now;
+            }
+
+            this.Dispatcher.Invoke(() => { tf_interation.Text = e.QuotioenTop.ToString(); });
+        }
+        private void _calculator_End(object? sender, CalcEvent e)
+        {
+            DispayResult(e);
+        }
+
+        private void DispayResult(CalcEvent e)
+        {
+            this.Dispatcher.Invoke(() =>
+            {
+                this.tf_interation.Text = e.Interation.ToString("#,##0");
+                if (chb_viewResult.IsChecked ?? false)
+                    this.tf_Result.Text = e.Result.ToString("#,##0");
+                tf_Digits.Text = e.Result.ToString().Length.ToString("#,##0");
+                td_Duration.Text = (DateTime.Now.Subtract(_start)).ToString();
+            });
         }
 
         private async void Button_Click(object sender, RoutedEventArgs e)
         {
-
             if (_cancellationTokenSource != null)
             {
                 _cancellationTokenSource.Cancel();
             }
             else
             {
-                sf_start.Content = "_Cancel";
+                _start = DateTime.Now;
+
+                tbStartStop.Text = "_Cancel";
+                tbStart.Visibility = Visibility.Collapsed;
+                tbStop.Visibility = Visibility.Visible;
+
                 _cancellationTokenSource = new CancellationTokenSource();
                 var token = _cancellationTokenSource.Token;
 
@@ -44,19 +87,21 @@ namespace NineToNineToNine
                 int w3 = int.Parse(tf_W3.Text);
 
                 tf_interation.Text = "starting"; // UI Thread
+
                 try
                 {
                     var folder = CreateAndGetFolder();
                     string filename = Path.Combine(folder, $"{w1} pow {w2} pow {w3}.txt");
 
-                    int n = 1;
+                    int startIteration = 1;
                     BigInteger sum = w1;
 
                     await Task.Run(() =>
-                        HeavyMethodAsync(
+                            _calculator.StartCalcualte(
                             w1,
                             w2,
-                            w3, n, sum, filename, token), token);
+                            w3, startIteration, sum, filename, token),
+                        token);
                 }
 
                 catch (OperationCanceledException ex)
@@ -67,7 +112,9 @@ namespace NineToNineToNine
                 {
                     _cancellationTokenSource.Dispose();
                     _cancellationTokenSource = null;
-                    sf_start.Content = "_Starten";
+                    tbStartStop.Text = "_Starten";
+                    tbStart.Visibility = Visibility.Visible;
+                    tbStop.Visibility = Visibility.Collapsed;
                 }
                 tf_EndTime.Text = DateTime.Now.ToString();
             }
@@ -84,11 +131,16 @@ namespace NineToNineToNine
         }
 
 
+        private void tf_W1_TextChanged(object sender, TextChangedEventArgs e)
+        {
+            if ((tf_W3 != null) && (tf_oberePotenz != null))
+                tf_oberePotenz.Text = Math.Pow(int.Parse(tf_W2.Text), int.Parse(tf_W3.Text)).ToString("#,##0");
+        }
 
         private void tf_W2_TextChanged(object sender, TextChangedEventArgs e)
         {
             if ((tf_W3 != null) && (tf_oberePotenz != null))
-                tf_oberePotenz.Text = Math.Pow(int.Parse(tf_W2.Text), int.Parse(tf_W3.Text)).ToString("0,000");
+                tf_oberePotenz.Text = Math.Pow(int.Parse(tf_W2.Text), int.Parse(tf_W3.Text)).ToString("#,##0");
         }
 
         private void chb_viewResult_Unchecked(object sender, RoutedEventArgs e)
@@ -101,5 +153,7 @@ namespace NineToNineToNine
         {
             Process.Start("explorer.exe", CreateAndGetFolder());
         }
+
+
     }
 }
